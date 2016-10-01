ba76dc33ea54525d3a6a6d29d440625cae7918cd
Gina White <ginabythebay@gmail.com>
2016-09-30 18:06:59 -0700

    seems to pretty much work?

--- cmd.go
+++ cmd.go
@@ -3,17 +3,199 @@ package main
 import (
 	"fmt"
 	"os"
+	"path"
+	"strings"
+	"sync"
+	"time"
 
+	"github.com/ginabythebay/poltroon/cower"
+	"github.com/pkg/errors"
 	"github.com/urfave/cli"
 )
+
+const dirMode = os.ModeDir | 0770
 
 func main() {
 	app := cli.NewApp()
 	app.Usage = "Foolishly upgrade AUR packages"
+	app.Flags = []cli.Flag{
+		cli.IntFlag{
+			Name:  "fetchers",
+			Value: 2,
+			Usage: "Number of concurrent fetchers",
+		},
+		cli.IntFlag{
+			Name:  "makers",
+			Value: 2,
+			Usage: "Number of concurrent makers",
+		},
+		cli.BoolFlag{
+			Name:  "skippgpcheck",
+			Usage: "Turn off pgp checks",
+		},
+	}
 	app.Action = func(c *cli.Context) error {
-		fmt.Println("boom! I say!")
+		start := time.Now()
+
+		root, err := getRoot()
+		if err != nil {
+			fatal(err)
+		}
+
+		cwr, err := cower.Find()
+		if err != nil {
+			fatal(err)
+		}
+		updates, err := cwr.QueryUpdates()
+		if err != nil {
+			fatal(err)
+		}
+
+		var waitGroup sync.WaitGroup
+
+		// TODO(gina) dump out the packages and ask the user if they want to continue
+
+		makeChan := make(chan *aurPackage)
+		for i := 0; i < c.Int("makers"); i++ {
+			go func() {
+				for pkg := range makeChan {
+					output(fmt.Sprintf("%s: beginning make...", pkg.name))
+					pkgPath, err := cwr.Make(pkg.build(), pkg.logs(), pkg.name, c.Bool("skippgpcheck"))
+					if err != nil {
+						output(fmt.Sprintf("%s: failed to make due to %+v", pkg.name, err))
+						waitGroup.Done()
+						continue
+					}
+					pkg.pkgPath = pkgPath
+					output(fmt.Sprintf("%s: successfully made", pkg.name))
+					waitGroup.Done()
+				}
+			}()
+		}
+
+		fetchChan := make(chan *aurPackage)
+		for i := 0; i < c.Int("fetchers"); i++ {
+			go func() {
+				for pkg := range fetchChan {
+					output(fmt.Sprintf("%s: beginning fetch...", pkg.name))
+					err := pkg.preparePackageDir()
+					if err != nil {
+						output(fmt.Sprintf("%s: failed to fetch due to %+v", pkg.name, err))
+						waitGroup.Done()
+						continue
+					}
+					err = cwr.Fetch(pkg.build(), pkg.logs(), pkg.name)
+					if err != nil {
+						output(fmt.Sprintf("%s: failed to fetch due to %+v", pkg.name, err))
+						waitGroup.Done()
+						continue
+					}
+					output(fmt.Sprintf("%s: successfully fetched", pkg.name))
+					makeChan <- pkg
+				}
+			}()
+		}
+
+		waitGroup.Add(len(updates))
+		aurPkgs := []*aurPackage{}
+		for _, u := range updates {
+			pkg := newAurPackage(root, u.Name)
+			aurPkgs = append(aurPkgs, pkg)
+			fetchChan <- pkg
+		}
+
+		waitGroup.Wait()
+
+		var good, bad []string
+		for _, pkg := range aurPkgs {
+			if pkg.pkgPath == "" {
+				bad = append(bad, pkg.name)
+			} else {
+				good = append(good, pkg.pkgPath)
+			}
+		}
+
+		elapsed := time.Since(start)
+
+		fmt.Println()
+		for _, b := range bad {
+			fmt.Printf("***Error processing %s***\n", b)
+		}
+		if len(bad) != 0 {
+			fmt.Println()
+		}
+
+		if len(bad) == 0 && len(good) == 0 {
+			fmt.Printf("Found nothing to do in %s\n", elapsed)
+		} else {
+			fmt.Printf("Created %d packages in %s\n", len(good), elapsed)
+		}
+
+		fmt.Printf("\nYou can now run:\n")
+		if len(good) != 0 {
+			fmt.Printf("    sudo pacman -U --noconfirm %s\n", strings.Join(good, " "))
+		}
+		fmt.Printf("    rm -rf %s/*\n", root)
+
 		return nil
 	}
 
 	app.Run(os.Args)
+}
+
+var outputMutex sync.Mutex
+
+func output(s string) {
+	outputMutex.Lock()
+	fmt.Println(s)
+	outputMutex.Unlock()
+}
+
+func fatal(v ...interface{}) {
+	fmt.Fprintln(os.Stderr, v...)
+	os.Exit(1)
+}
+
+func getRoot() (string, error) {
+	root := path.Join(os.TempDir(), "poltroon")
+	err := os.MkdirAll(root, dirMode)
+	return root, err
+}
+
+type aurPackage struct {
+	// name of the package
+	name string
+	// root of the package directory
+	root string
+
+	// set after a successful make
+	pkgPath string
+}
+
+func newAurPackage(root, pkg string) *aurPackage {
+	return &aurPackage{
+		name: pkg,
+		root: path.Join(root, pkg),
+	}
+}
+
+func (p *aurPackage) preparePackageDir() error {
+	if err := os.RemoveAll(p.root); err != nil {
+		return errors.Wrapf(err, "Unable to clean %q", p.root)
+	}
+	if err := os.MkdirAll(p.logs(), dirMode); err != nil {
+		return err
+	}
+	if err := os.MkdirAll(p.build(), dirMode); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (p *aurPackage) logs() string {
+	return path.Join(p.root, "logs")
+}
+
+func (p *aurPackage) build() string {
+	return path.Join(p.root, "build")
 }
---
+++ cower/cower.go
@@ -0,0 +1,174 @@
+package cower
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"os/exec"
+	"path"
+	"path/filepath"
+	"syscall"
+
+	"github.com/pkg/errors"
+)
+
+type Cower struct {
+	cowerPath   string
+	makePkgPath string
+}
+
+func findPgm(pgm string) (p string, err error) {
+	p, err = exec.LookPath(pgm)
+	if err != nil {
+		return "", errors.Wrapf(err, "Unable to find %s command.", pgm)
+	}
+	if !path.IsAbs(p) {
+		curDir, err := os.Getwd()
+		if err != nil {
+			return "", errors.Wrapf(err, "Unable to determine current working directory, so we cannot build path to %s command", pgm)
+		}
+		p = path.Join(curDir, p)
+	}
+	return
+}
+
+// Find finds the path to the cower command.
+func Find() (*Cower, error) {
+	cowerPath, err := findPgm("cower")
+	if err != nil {
+		return nil, errors.Wrap(err, "Look for cower here: https://aur.archlinux.org/packages/cower/")
+	}
+
+	makePkgPath, err := findPgm("makepkg")
+	if err != nil {
+		return nil, err
+	}
+
+	return &Cower{cowerPath, makePkgPath}, nil
+}
+
+func (c *Cower) QueryUpdates() ([]Package, error) {
+	cmd := exec.Command(c.cowerPath, "--update")
+	cmd.Stderr = os.Stderr
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return nil, errors.Wrap(err, "Getting stdout for cower --update")
+	}
+	if err = cmd.Start(); err != nil {
+		return nil, errors.Wrap(err, "starting cower --update")
+	}
+	pkgs := []Package{}
+	scanner := bufio.NewScanner(stdout)
+	for scanner.Scan() {
+		line := scanner.Text()
+		// Example line:
+		// :: google-chrome 53.0.2785.116-1 -> 53.0.2785.143-1
+		var colons, name, curver, arrow, newver string
+		_, err = fmt.Sscan(line, &colons, &name, &curver, &arrow, &newver)
+		if err != nil {
+			return nil, errors.Wrapf(err, "Unable to parse [%s]", line)
+		}
+		pkgs = append(pkgs, Package{name, curver, newver})
+	}
+	err = cmd.Wait()
+	// an exit code of 1 is normal if cower found something to update
+	if status, ok := exitStatus(err); !ok || (status != 0 && status != 1) {
+		return nil, errors.Wrap(err, "executing cower --update")
+	}
+	return pkgs, nil
+}
+
+func (c *Cower) Fetch(dir string, logDir string, name string) error {
+	cmd := exec.Command(c.cowerPath, "--download", name)
+	cmd.Dir = dir
+	stdout, err := os.Create(path.Join(logDir, "fetch.out"))
+	if err != nil {
+		return errors.Wrapf(err, "Fetching %s", name)
+	}
+	defer stdout.Close()
+	cmd.Stdout = stdout
+
+	stderr, err := os.Create(path.Join(logDir, "fetch.err"))
+	if err != nil {
+		return errors.Wrapf(err, "Fetching %s", name)
+	}
+	defer stderr.Close()
+	cmd.Stderr = stderr
+
+	if err = cmd.Run(); err != nil {
+		return errors.Wrapf(err, "running cower --download %s.  See %s", name, logDir)
+	}
+	return nil
+}
+
+func (c *Cower) Make(dir string, logDir string, name string, skippgpcheck bool) (pkgPath string, err error) {
+	cmd := exec.Command(c.makePkgPath, "--syncdeps", name)
+	if skippgpcheck {
+		cmd.Args = append(cmd.Args, "--skippgpcheck")
+	}
+	cmd.Dir = path.Join(dir, name)
+	stdout, err := os.Create(path.Join(logDir, "make.out"))
+	if err != nil {
+		return "", errors.Wrapf(err, "Making %s", name)
+	}
+	defer stdout.Close()
+	cmd.Stdout = stdout
+
+	stderr, err := os.Create(path.Join(logDir, "make.err"))
+	if err != nil {
+		return "", errors.Wrapf(err, "Making %s", name)
+	}
+	defer stderr.Close()
+	cmd.Stderr = stderr
+
+	if err = cmd.Run(); err != nil {
+		return "", errors.Wrapf(err, "running makepkg for %s.  See %s", name, logDir)
+	}
+
+	matches, err := filepath.Glob(path.Join(cmd.Dir, "*.pkg.*"))
+	if err != nil {
+		return "", errors.Wrapf(err, "globbing makepkg for %s.  See %s", name, cmd.Dir)
+	}
+	if len(matches) != 1 {
+		return "", errors.Errorf("Expected exactly 1 match but got this instead: %v", matches)
+	}
+
+	return matches[0], nil
+}
+
+// Package represents what we know about a package that can be updated
+type Package struct {
+	// Name is the name of this package (e.g. 'google-chrome')
+	Name string
+	// CurrentVersion is the version of this package currently installed.
+	CurrentVersion string
+	// NextVersion is the available version of this package
+	NextVersion string
+}
+
+func (p Package) String() string {
+	return fmt.Sprintf(":: %s %s -> %s", p.Name, p.CurrentVersion, p.NextVersion)
+}
+
+// see http://stackoverflow.com/questions/10385551/get-exit-code-go
+func exitStatus(err error) (status int, ok bool) {
+	if err == nil {
+		return 0, true
+	}
+
+	exiterr, ok := err.(*exec.ExitError)
+	if !ok {
+		return 0, false
+	}
+	// The program has exited with an exit code != 0
+
+	// This works on both Unix and Windows. Although package
+	// syscall is generally platform dependent, WaitStatus is
+	// defined for both Unix and Windows and in both cases has
+	// an ExitStatus() method with the same signature.
+	waitStatus, ok := exiterr.Sys().(syscall.WaitStatus)
+	if !ok {
+		return 0, false
+	}
+	return waitStatus.ExitStatus(), true
+}
