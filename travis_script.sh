60e3a6b38235aaf401029361fcf595789053970f
Gina White <ginabythebay@gmail.com>
2016-10-03 16:47:49 -0700

    fix linker flags
    also more ls

--- travis_script.sh
+++ travis_script.sh
@@ -17,10 +17,11 @@ cd $TRAVIS_BUILD_DIR
 headerdir="$TRAVIS_BUILD_DIR/pacman/lib/libalpm"
 libdir="$TRAVIS_BUILD_DIR/pacman/lib/libalpm/"
 export CGO_CFLAGS="$CGO_CFLAGS -I${headerdir}"
-export LDFLAGS="$LDFLAGS -L${libdir}"
+export CGO_LDFLAGS="$CGO_LDFLAGS -L${libdir}"
 
 # let me see what it set up here
-ls pacman/lib/libalpm/*/*
+ls -l pacman/lib/libalpm/*
+ls -l pacman/lib/libalpm/.libs/*
 env
 
 go get -t -v ./...
