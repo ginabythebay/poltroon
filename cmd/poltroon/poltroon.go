98e8e9c0b2db5f8fd746bee74c69d2ea1acbe1e3
Gina White <ginabythebay@gmail.com>
2016-10-08 12:37:29 -0700

    bring usage statement up to date

--- cmd/poltroon/poltroon.go
+++ cmd/poltroon/poltroon.go
@@ -37,11 +37,12 @@ func main() {
 	app.Usage = strings.TrimSpace(`
 Foolishly upgrade AUR packages.
 
-1. Runs cower -u to find already-installed packages that are out of
-   date, prints out the results.
+1. Finds foreign packages by running packman --query --foreign and
+   then consults the AUR for newer versions.  Prints out all packages
+   found to have newer versions.
 2. Asks if the user wants to proceed.  Exits if they don't.
 3. Starts a two-stage pipeline.
-4. In the first stage, we run cower -d to download the package (default it two workers).
+4. In the first stage, we run download the package (defaulting it 2 workers).
 5. In the second state, we run makepkg -s to build the package files.
 6. At the end, we print out the command the user can run to install the packages.
 
